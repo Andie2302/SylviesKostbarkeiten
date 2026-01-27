2eeb1f28f6aa2d9488c182cbe3524d5daac5a75f
andreas <Andreas.Zwander@gmail.com>
2026-01-27 15:27:28 +0100

    Add method to increase article prices by a percentage in Tools utility class

--- SylviesKostbarkeiten/Tools.cs
+++ SylviesKostbarkeiten/Tools.cs
@@ -8,4 +8,16 @@ public static class Tools
     public static string DesktopPfad => Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
     public static string GetDesktopDatei(string dateiName) => Path.Combine(DesktopPfad, dateiName);
     public static List<KassenGruppenInfo> LeseMenueDatei(string menueDateiPfad) => File.Exists(menueDateiPfad) ? new MenueParser().Parse(menueDateiPfad) : [];
+
+    public static List<KassenArtikelInfo> ErhöhePreisUm(KassenGruppenInfo gruppenInfo, decimal erhoehungProzent)
+    {
+        return gruppenInfo.Artikel.Select(a =>
+        {
+            if (a.Vat <= 0) return a;
+            var neuerPreis = a.Price * (1 + erhoehungProzent / 100);
+            neuerPreis = Math.Round(neuerPreis * 10, MidpointRounding.AwayFromZero) / 10;
+            return a with { Price = neuerPreis };
+        }).ToList();
+    }
+    
 }
