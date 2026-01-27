64f9787cd0848323d3b6c531e996ebba2d5f65e7
andreas <Andreas.Zwander@gmail.com>
2026-01-27 12:05:30 +0100

    Refactor VerkaufsParser to streamline sales data parsing logic

--- SylviesKostbarkeiten/io/VerkaufsParser.cs
+++ SylviesKostbarkeiten/io/VerkaufsParser.cs
@@ -7,41 +7,10 @@ namespace SylviesKostbarkeiten.io;
 public class VerkaufsParser
 {
     private static readonly CultureInfo GermanCulture = new CultureInfo("de-AT");
-
     public List<VerkaufsPositionInfo> Parse(string filePath)
     {
-        var positionen = new List<VerkaufsPositionInfo>();
-        // Header überspringen
         var lines = File.ReadAllLines(filePath, Encoding.UTF8).Skip(1);
-
-        foreach (var line in lines)
-        {
-            if (string.IsNullOrWhiteSpace(line)) continue;
-
-            var p = line.Split(';');
-            
-            // Sicherheit: Falls die Zeile unvollständig ist
-            if (p.Length < 20) continue;
-
-            positionen.Add(new VerkaufsPositionInfo(
-                BelegNr: p[0],
-                Zeitstempel: DateTime.ParseExact(p[1], "dd.MM.yy HH:mm", GermanCulture),
-                KassaBezeichnung: p[4],
-                ArtikelName: p[7],
-                Gruppe: p[9],
-                MwStSatz: ParseDecimal(p[11]),
-                PreisProStk: ParseDecimal(p[12]),
-                Menge: ParseDecimal(p[13]),
-                MwStEuro: ParseDecimal(p[17]),
-                Netto: ParseDecimal(p[18]),
-                Brutto: ParseDecimal(p[19]),
-                BezahlArt: p[16]
-            ));
-        }
-
-        return positionen;
+        return (from line in lines where !string.IsNullOrWhiteSpace(line) select line.Split(';') into p where p.Length >= 20 select new VerkaufsPositionInfo(BelegNr: p[0], Zeitstempel: DateTime.ParseExact(p[1], "dd.MM.yy HH:mm", GermanCulture), KassaBezeichnung: p[4], ArtikelName: p[7], Gruppe: p[9], MwStSatz: ParseDecimal(p[11]), PreisProStk: ParseDecimal(p[12]), Menge: ParseDecimal(p[13]), MwStEuro: ParseDecimal(p[17]), Netto: ParseDecimal(p[18]), Brutto: ParseDecimal(p[19]), BezahlArt: p[16])).ToList();
     }
-
-    private decimal ParseDecimal(string value) =>
-        decimal.TryParse(value.Trim('"'), NumberStyles.Any, GermanCulture, out var res) ? res : 0m;
+    private decimal ParseDecimal(string value) => decimal.TryParse(value.Trim('"'), NumberStyles.Any, GermanCulture, out var res) ? res : 0m;
 }
