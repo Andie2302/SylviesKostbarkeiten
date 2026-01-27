dfd72880e0ed176b912d6799ae85c7d753d67e81
andreas <Andreas.Zwander@gmail.com>
2026-01-27 12:04:47 +0100

    Add VerkaufsParser to parse sales data from CSV files

---
+++ SylviesKostbarkeiten/io/VerkaufsParser.cs
@@ -0,0 +1,47 @@
+using System.Globalization;
+using System.Text;
+using SylviesKostbarkeiten.models;
+
+namespace SylviesKostbarkeiten.io;
+
+public class VerkaufsParser
+{
+    private static readonly CultureInfo GermanCulture = new CultureInfo("de-AT");
+
+    public List<VerkaufsPositionInfo> Parse(string filePath)
+    {
+        var positionen = new List<VerkaufsPositionInfo>();
+        // Header überspringen
+        var lines = File.ReadAllLines(filePath, Encoding.UTF8).Skip(1);
+
+        foreach (var line in lines)
+        {
+            if (string.IsNullOrWhiteSpace(line)) continue;
+
+            var p = line.Split(';');
+            
+            // Sicherheit: Falls die Zeile unvollständig ist
+            if (p.Length < 20) continue;
+
+            positionen.Add(new VerkaufsPositionInfo(
+                BelegNr: p[0],
+                Zeitstempel: DateTime.ParseExact(p[1], "dd.MM.yy HH:mm", GermanCulture),
+                KassaBezeichnung: p[4],
+                ArtikelName: p[7],
+                Gruppe: p[9],
+                MwStSatz: ParseDecimal(p[11]),
+                PreisProStk: ParseDecimal(p[12]),
+                Menge: ParseDecimal(p[13]),
+                MwStEuro: ParseDecimal(p[17]),
+                Netto: ParseDecimal(p[18]),
+                Brutto: ParseDecimal(p[19]),
+                BezahlArt: p[16]
+            ));
+        }
+
+        return positionen;
+    }
+
+    private decimal ParseDecimal(string value) =>
+        decimal.TryParse(value.Trim('"'), NumberStyles.Any, GermanCulture, out var res) ? res : 0m;
+}
