6da6c5d8cc5695fee471706a10091b081fb263f6
andreas <Andreas.Zwander@gmail.com>
2026-01-27 11:41:05 +0100

    Read and parse menu groups from "quelle.csv" on desktop using MenueParser

--- SylviesKostbarkeiten/Program.cs
+++ SylviesKostbarkeiten/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using SylviesKostbarkeiten.io;
+
+Console.WriteLine("Lese Kassen-Menü ein...");
+
+string desktopPfad = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+string dateiPfad = Path.Combine(desktopPfad, "quelle.csv");
+
+var parser = new MenueParser();
+
+if (File.Exists(dateiPfad))
+{
+    var gruppen = parser.Parse(dateiPfad);
+    foreach (var gruppe in gruppen)
+    {
+        Console.WriteLine($"Gruppe: {gruppe.Name} mit {gruppe.Artikel.Count} Artikeln.");
+    }
+}
+else
+{
+    Console.WriteLine($"Datei nicht gefunden unter: {dateiPfad}");
+}
